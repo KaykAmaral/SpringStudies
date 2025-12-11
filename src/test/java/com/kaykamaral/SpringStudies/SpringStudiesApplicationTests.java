8b26656e7e35814e740cf314ec9d1a595bbdfa05
KaykAmaral <kayaquiurbano@gmail.com>
2025-12-11 11:51:08 -0300

    Initial commit

---
+++ src/test/java/com/kaykamaral/SpringStudies/SpringStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaykamaral.SpringStudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
